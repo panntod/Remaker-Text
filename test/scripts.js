b4081a07baa746125b60e9a5d2beee247ae04861
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-19 13:10:24 +0700

    chore: fixed cannot replace text, added function copy to clipboard

--- test/scripts.js
+++ test/scripts.js
@@ -1,23 +1,46 @@
-let teksAwal = document.getElementById('textToReplace');
+let teksAwal = document.getElementById("textToReplace");
 
-document.getElementById("inputText").addEventListener("submit", function (event) {
+document
+  .getElementById("inputText")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let inputText = document.getElementById('teksAwal').value;
+    let inputText = document.getElementById("teksAwal").value;
     teksAwal.textContent = inputText;
-});
+  });
 
 function findAllText() {
-    let findWord = document.getElementById("findText").value;
-    let regex = new RegExp("\\b" + findWord + "\\b", 'g');
-    let newText = teksAwal.textContent.replace(regex, '<span style="background-color: yellow;">' + findWord + '</span>');
-    teksAwal.innerHTML = newText;
+  let findWord = document.getElementById("findText").value;
+  let regex = new RegExp("\\b" + findWord + "\\b", "g");
+  let newText = teksAwal.textContent.replace(
+    regex,
+    '<span style="background-color: yellow;">' + findWord + "</span>"
+  );
+  teksAwal.innerHTML = newText;
 }
 
 function replaceAllText() {
-    let findWord = document.getElementById("findText").value;
-    let replaceText = document.getElementById("replacementInput").value;
-    let regex = new RegExp("\\b" + findWord + "\\b", 'g');
-    let newText = teksAwal.textContent.replace(regex, replaceText);
-    teksAwal.textContent = newText;
+  let findWord = document.getElementById("findText").value;
+  let replaceText = document.getElementById("replacementInput").value;
+  let regex = new RegExp("\\b" + findWord + "\\b", "g");
+  let newText = teksAwal.textContent.replace(regex, replaceText);
+  teksAwal.innerHTML = newText;
 }
+
+document.getElementById("copyButton").addEventListener("click", function () {
+  if (!teksAwal.innerHTML) {
+    console.error("Tidak ada teks yang dapat disalin.");
+    alert("Tidak ada teks yang dapat disalin");
+    return;
+  }
+
+  navigator.clipboard
+    .writeText(teksAwal.innerHTML)
+    .then(() => {
+      alert("Teks berhasil disalin!");
+    })
+    .catch((err) => {
+      console.error("Gagal menyalin teks: ", err);
+      alert("Gagal menyalin teks");
+    });
+});
